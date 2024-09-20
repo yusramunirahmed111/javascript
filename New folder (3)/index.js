877a0987c2e4431494d8850bb1cc8f1d28591b86
yusramunirahmed111 <sairayusra.2811@gmail.com>
2024-09-20 16:18:37 +0500

    Add files via upload

---
+++ New folder (3)/index.js
@@ -0,0 +1,20 @@
+var myName = "yusra";
+alert(myName);
+var myName = "yusra";
+var fullName="munir ahmed";
+console.log(myName+ " " + fullName);
+var a = 4;
+var b = 2;
+console.log(a++ + --b - --a + b++ + a+b);
+var num1 = 4;
+var num2 = 5;
+var sum  =  num1 + num2;
+var user=prompt("Solve the statement" + " " + 4 + " " +"+" + 5)
+if (user == 9) {
+    console.log("Yes your ans is right!");
+}else if (user != 9){
+    console.log("Your ans is wrong!");
+
+}else{
+    console.log("Try again!")
+}
